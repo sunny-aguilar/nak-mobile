c7c814e7713912fc124e617dd423ec9beef61841
Sandro Aguilar <sunny.aguilar@gmail.com>
2024-04-05 13:07:24 -0700

    nak-app

--- lib/components/featured_stories.dart
+++ lib/components/featured_stories.dart
@@ -11,52 +11,54 @@ class HomeScreenChildren extends StatelessWidget {
   const HomeScreenChildren({super.key});
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: db.UserService().getData(),
-      builder: (BuildContext context, snapshot) {
-        if (snapshot.hasError) {
-          return const Center(child:Text('Error building home screen'));
-        }
-        else if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasError) {
-            print('snapshot.hasError 1');
-            return Center(
-              child: Text('An ${snapshot.error} occurred'),
-            );
-          }
-          else if (snapshot.hasData) {
-            // return the listview holding the homepage elements
-            print('snapshot.hasError 2');
-            return const BlogStream();
-          }
-        }
-        else {
-          print('snapshot.hasError 3');
-          return const Center(
-            child: SizedBox(
-              height: 50,
-              width: 50,
-              child: CircularProgressIndicator(
-                strokeWidth: 10,
-              ),
-            ),
-          );
-        }
-        // display default value
-        print('snapshot.hasError 4');
-        return const Center(
-          child: SizedBox(
-            height: 150,
-              width: 150,
-            child: CircularProgressIndicator(
-              color: theme.azureClr,
-            ),
-          ),
-        );
-      },
-    );
+    return const BlogStream();
+    // return FutureBuilder(
+    //   future: db.UserService().getData(),
+    //   builder: (BuildContext context, snapshot) {
+    //     if (snapshot.hasError) {
+    //       return const Center(child:Text('Error building home screen'));
+    //     }
+    //     else if (snapshot.connectionState == ConnectionState.done) {
+    //       if (snapshot.hasError) {
+    //         print('snapshot.hasError 1');
+    //         return Center(
+    //           child: Text('An ${snapshot.error} occurred'),
+    //         );
+    //       }
+    //       else if (snapshot.hasData) {
+    //         // return the listview holding the homepage elements
+    //         print('snapshot.hasError 2');
+    //         return const BlogStream();
+    //       }
+    //     }
+    //     else {
+    //       print('snapshot.hasError 3');
+    //       return const Center(
+    //         child: SizedBox(
+    //           height: 50,
+    //           width: 50,
+    //           child: CircularProgressIndicator(
+    //             strokeWidth: 10,
+    //           ),
+    //         ),
+    //       );
+    //     }
+    //     // display default value
+    //     print('snapshot.hasError 4');
+    //     return const Center(
+    //       child: SizedBox(
+    //         height: 150,
+    //           width: 150,
+    //         child: CircularProgressIndicator(
+    //           color: theme.azureClr,
+    //         ),
+    //       ),
+    //     );
+    //   },
+    // );
   }
 }
+
 
 
 final ScrollController controller = ScrollController();
@@ -69,6 +71,7 @@ void scrollUp() {
 }
 
 
+
 class BlogStream extends StatefulWidget {
   const BlogStream({super.key});
   @override
@@ -79,102 +82,108 @@ class BlogStream extends StatefulWidget {
 class _BlogStreamState extends State<BlogStream> {
   final Stream<QuerySnapshot> _stream = FirebaseFirestore.instance.collection('blog').orderBy('date', descending: true).snapshots();
 
-  CircularProgressIndicator circularProgress() {
-    return const CircularProgressIndicator(
-      strokeWidth: 5,
-      color: theme.azureClr,
-      backgroundColor: theme.greyClr,
+  Center circularProgress() {
+    return const Center(
+      child: SizedBox(
+        height: 150,
+        width: 150,
+        child: CircularProgressIndicator(
+          strokeWidth: 5,
+          color: theme.azureClr,
+          backgroundColor: theme.greyClr,
+        ),
+      ),
     );
   }
 
-  ListView buildBlogList(snapshot) {
+  // ListView buildBlogList(snapshot) {
     // list to hold carousel
-    List<Widget> carouselList = [
-      const carousel.CarouselComponent(),
-      const SizedBox(height: 4,),
-    ];
+    // List<Widget> carouselList = [
+    //   const carousel.CarouselComponent(),
+    //   const SizedBox(height: 4,),
+    // ];
 
     // get blog data, create card templates, save to list
-    List<Widget> blogList = snapshot.data!.docs.map<Widget>( (DocumentSnapshot document) {
-      String defaultUserImg = 'https://firebasestorage.googleapis.com/v0/b/nak-app-a899e.appspot.com/o/selfies%2Fprofile.webp?alt=media&token=9a3346e1-069e-4878-aa43-54394a368a5e';
+    // List<Widget> blogList = snapshot.data!.docs.map<Widget>( (DocumentSnapshot document) {
+    //   String defaultUserImg = 'https://firebasestorage.googleapis.com/v0/b/nak-app-a899e.appspot.com/o/selfies%2Fprofile.webp?alt=media&token=9a3346e1-069e-4878-aa43-54394a368a5e';
 
-      // get data from blog snapshot
-      Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-
-
-      return GestureDetector(
-        onTap: () {
-          /* go to page */
-          /* might want to add edit/delete options */
-        },
-
-        child: FutureBuilder(
-          future: db.BlogDB(docID: data['docID']).getBlogUser(),
-          builder: (BuildContext context, snapshot) {
-            if (snapshot.hasError) {
-              return const Center(child: Text('Error building home screen'),);
-            }
-            else if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasError) {
-                return Center(
-                  child: Text(
-                    'An ${snapshot.error} occurred. Please notify us at developer@nakinc.org.'
-                  ),
-                );
-              }
-              else if (snapshot.hasData) {
-                // return the listview holding the homepage elements
-                final dataforUser = snapshot.data;
-                return cards.StoryCardNetwork(
-                  userImage: dataforUser!['selfie'] ?? defaultUserImg,      // userData is comes from FutureBuilder
-                  userName: data['name'],
-                  storyHeadline: data['title'],
-                  image: data['url'],
-                  storyText: data['body'],
-                  date: data['date'],
-                );
-              }
-            }
-            else {
-              return const Center(
-                child: SizedBox(
-                  height: 50,
-                  width: 50,
-                  child: CircularProgressIndicator(
-                    strokeWidth: 10,
-                  ),
-                ),
-              );
-            }
-            // display default value
-            return const Center(
-              child: SizedBox(
-                height: 150,
-                  width: 150,
-                child: CircularProgressIndicator(
-                  color: theme.azureClr,
-                ),
-              ),
-            );
+    //   // get data from blog snapshot
+    //   Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
 
 
-          },
-        ),
-      );
-    }).toList();
+    //   return GestureDetector(
+    //     onTap: () {
+    //       /* go to page */
+    //       /* might want to add edit/delete options */
+    //     },
+
+    //     child: FutureBuilder(
+    //       future: db.BlogDB(docID: data['docID']).getBlogUser(),
+    //       builder: (BuildContext context, snapshot) {
+    //         if (snapshot.hasError) {
+    //           return const Center(child: Text('Error building home screen'),);
+    //         }
+    //         else if (snapshot.connectionState == ConnectionState.done) {
+    //           if (snapshot.hasError) {
+    //             return Center(
+    //               child: Text(
+    //                 'An ${snapshot.error} occurred. Please notify us at developer@nakinc.org.'
+    //               ),
+    //             );
+    //           }
+    //           else if (snapshot.hasData) {
+    //             // return the listview holding the homepage elements
+    //             final dataforUser = snapshot.data;
+    //             return cards.StoryCardNetwork(
+    //               userImage: dataforUser!['selfie'] ?? defaultUserImg,      // userData is comes from FutureBuilder
+    //               userName: data['name'],
+    //               storyHeadline: data['title'],
+    //               image: data['url'],
+    //               storyText: data['body'],
+    //               date: data['date'],
+    //             );
+    //           }
+    //         }
+    //         else {
+    //           return const Center(
+    //             child: SizedBox(
+    //               height: 50,
+    //               width: 50,
+    //               child: CircularProgressIndicator(
+    //                 strokeWidth: 10,
+    //               ),
+    //             ),
+    //           );
+    //         }
+    //         // display default value
+    //         return const Center(
+    //           child: SizedBox(
+    //             height: 150,
+    //               width: 150,
+    //             child: CircularProgressIndicator(
+    //               color: theme.azureClr,
+    //             ),
+    //           ),
+    //         );
+
+
+    //       },
+    //     ),
+    //   );
+    // }).toList();
 
 
     // combine the lists
-    List<Widget> combinedList = carouselList + blogList;
+    // List<Widget> combinedList = carouselList + blogList;
 
     // create a listview for the list
-    ListView myList = ListView(
-      children: combinedList,
-    );
+    // ListView myList = ListView(
+    //   children: combinedList,
+    // );
 
     // return the list
-    return myList;
-  }
+    // return myList;
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -182,36 +191,207 @@ class _BlogStreamState extends State<BlogStream> {
       stream: _stream,
       // initialData: 'string',
       builder: (BuildContext context, AsyncSnapshot snapshot) {
-        // check for errors
-        // if (snapshot.connectionState == ConnectionState.waiting) {
-        //   return circularProgress();
-        // }
-        // else if (snapshot.connectionState == ConnectionState.active) {
-        //   if (snapshot.hasError) {
-        //     return const Center(child: Text('Oh no! An error occurred!'),);
-        //   }
-        //   else if (!snapshot.hasData) {
-        //     return circularProgress();
-        //   }
-        //   else if (snapshot.hasData) {
-        //     return buildBlogList(snapshot);
-        //   }
-        // }
-        // return Text('State: ${snapshot.connectionState}');
+        String defaultUserImg = 'https://firebasestorage.googleapis.com/v0/b/nak-app-a899e.appspot.com/o/selfies%2Fprofile.webp?alt=media&token=9a3346e1-069e-4878-aa43-54394a368a5e';
+        List<Widget> clientWidgets = [];
 
-        switch (snapshot.connectionState) {
-          case ConnectionState.none:
-          case ConnectionState.waiting:
-            return circularProgress();
-          case ConnectionState.active:
-            return buildBlogList(snapshot);
-          case ConnectionState.done:
-            return const Center(child: Text('Oh no! An error occurred!'),);
+        // add carousel to list
+        List<Widget> carouselList = [
+          const carousel.CarouselComponent(),
+          const SizedBox(height: 4,),
+        ];
+
+        clientWidgets += carouselList;
+
+        if (snapshot.data == null) { return circularProgress(); }
+
+        if (!snapshot.hasData) { return circularProgress(); }
+
+        if (snapshot.hasData) {
+          final blogs = snapshot.data.docs.toList();
+          for(var blog in blogs) {
+            // var selfieUrl = db.BlogDB(docID: blogs['docID']).getBlogUser();
+            // print('Selfie UR: ${selfieUrl}');
+
+            // FutureBuilder(
+            //   future: db.BlogDB(docID: blogs['docID']).getBlogUser(),
+            //   // future: Future.delayed(const Duration(seconds: 1), () {return 1;}),
+            //   initialData: 'Empty String',
+            //   builder: (BuildContext context, snap) {
+            //     if (snap.connectionState == ConnectionState.waiting) {
+            //       return const Center(
+            //         child: SizedBox(
+            //           height: 150,
+            //             width: 150,
+            //           child: CircularProgressIndicator(
+            //             color: theme.azureClr,
+            //           ),
+            //         ),
+            //       );
+            //     }
+            //     if (snap.connectionState == ConnectionState.done) {
+            //       if (snap.hasError) {
+            //         return const Center(child: Text('Error building home screen'),);
+            //       }
+            //       else if (snap.hasData) {
+            //         // get url and set string variable
+            //         // final dataforUser = snap.data;
+            //         print('ERROR: ${snap.data}');
+            //         // selfieUrl = dataforUser?['selfie'];
+            //         // selfieUrl = dataforUser.toString();
+            //       }
+            //     }
+            //     // display default value
+            //     return const Center(
+            //       child: SizedBox(
+            //         height: 150,
+            //           width: 150,
+            //         child: CircularProgressIndicator(
+            //           color: theme.azureClr,
+            //         ),
+            //       ),
+            //     );
+            //   },
+            // );
+
+            final clientWidget =
+                cards.StoryCardNetwork(
+                  userImage: defaultUserImg,      // userData comes from FutureBuilder
+                  // userImage: defaultUserImg,      // userData is comes from FutureBuilder
+                  userName: blog['name'],
+                  storyHeadline: blog['title'],
+                  image: blog['url'],
+                  storyText: blog['body'],
+                  date: blog['date'],
+                );
+
+            clientWidgets.add(clientWidget);
+          }
         }
+
+        return ListView(
+          children: clientWidgets,
+        );
+
+        // switch (snapshot.connectionState) {
+        //   case ConnectionState.none:
+        //   case ConnectionState.waiting:
+        //     return circularProgress();
+        //   case ConnectionState.active:
+        //     // return buildBlogList(snapshot);
+        //     List<Widget> carouselList = [
+        //       const carousel.CarouselComponent(),
+        //       const SizedBox(height: 4,),
+        //     ];
+
+
+
+        //     // solution #2 with "red screen glitch"
+        //     // build a list of cards
+        //     List<Widget> blogList = snapshot.data!.docs.map<Widget>( (DocumentSnapshot document) {
+
+        //       // default image string to use if none has been selected
+        //       String defaultUserImg = 'https://firebasestorage.googleapis.com/v0/b/nak-app-a899e.appspot.com/o/selfies%2Fprofile.webp?alt=media&token=9a3346e1-069e-4878-aa43-54394a368a5e';
+
+        //       // prints data in Map
+        //       // print('data++++++++++++++++++++++++++++++++++++++++ \n${document.data()}');
+
+        //       // get data from blog snapshot
+        //       Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+
+        //       // return a card from Streambuilder that is added to a list
+        //       return GestureDetector(
+        //         onTap: () {
+        //           /* go to page */
+        //           /* might want to add edit/delete options */
+        //         },
+
+        //         child: FutureBuilder(
+        //           future: db.BlogDB(docID: data['docID']).getBlogUser(),
+        //           builder: (BuildContext context, snapshot) {
+        //             if (snapshot.hasError) {
+        //               return const Center(child: Text('Error building home screen'),);
+        //             }
+        //             else if (snapshot.connectionState == ConnectionState.done) {
+        //               if (snapshot.hasError) {
+        //                 return Center(
+        //                   child: Text(
+        //                     'An ${snapshot.error} occurred. Please notify us at developer@nakinc.org.'
+        //                   ),
+        //                 );
+        //               }
+        //               else if (snapshot.hasData) {
+        //                 // return the listview holding the homepage elements
+        //                 final dataforUser = snapshot.data;
+        //                 return cards.StoryCardNetwork(
+        //                   userImage: dataforUser!['selfie'] ?? defaultUserImg,      // userData is comes from FutureBuilder
+        //                   userName: data['name'],
+        //                   storyHeadline: data['title'],
+        //                   image: data['url'],
+        //                   storyText: data['body'],
+        //                   date: data['date'],
+        //                 );
+        //               }
+        //             }
+        //             else {
+        //               return const Center(
+        //                 child: SizedBox(
+        //                   height: 50,
+        //                   width: 50,
+        //                   child: CircularProgressIndicator(
+        //                     strokeWidth: 10,
+        //                   ),
+        //                 ),
+        //               );
+        //             }
+        //             // display default value
+        //             return const Center(
+        //               child: SizedBox(
+        //                 height: 150,
+        //                   width: 150,
+        //                 child: CircularProgressIndicator(
+        //                   color: theme.azureClr,
+        //                 ),
+        //               ),
+        //             );
+
+
+        //           },
+        //         ),
+        //       );
+        //     }).toList();
+
+        //     // combine the lists
+        //     List<Widget> combinedList = carouselList + blogList;
+
+        //     // create a listview for the list
+        //     ListView myList = ListView(
+        //       children: combinedList,
+        //     );
+
+        //     // return the list
+        //     return myList;
+
+        //   case ConnectionState.done:
+        //     return const Center(child: Text('Oh no! An error occurred!'),);
+        // }
       }
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class BuildBlog extends StatelessWidget {
   const BuildBlog({super.key});
