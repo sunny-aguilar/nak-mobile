91374cccd1cbaa2b43fc68d1360edd83ffe39ed9
Sandro Aguilar <sunny.aguilar@gmail.com>
2024-04-08 00:07:52 -0700

    nak-app

---
+++ lib/screens/admin/users.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:nak_app/db/db_ops.dart' as db;
+import 'package:nak_app/services/theme_service.dart' as service;
+
+class AllUsers extends StatefulWidget {
+  const AllUsers({super.key});
+
+  @override
+  State<AllUsers> createState() => _AllUsersState();
+}
+
+class _AllUsersState extends State<AllUsers> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Image.asset('assets/img/nak_letters_bw.png', height: 30.0,),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        actions: <Widget>[
+          IconButton(
+            icon: Get.isDarkMode ? const Icon(Icons.wb_sunny_outlined) : const Icon(Icons.dark_mode_outlined),
+            onPressed: () {
+              service.ThemeService().switchTheme();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
