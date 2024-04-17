61005a3574291af559c87c840f61b9bb8b934342
Sandro Aguilar <sunny.aguilar@gmail.com>
2024-04-16 18:13:05 -0700

    nak-app

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -14,21 +14,6 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   centerTitle: true,
-      //   title: Image.asset('assets/img/nak_letters_bw.png', height: 30.0,),
-      //   backgroundColor: Theme.of(context).colorScheme.primary,
-      //   actions: <Widget>[
-      //     // const Icon(Icons.message_outlined),
-      //     // const SizedBox(width: 10,),
-      //     IconButton(
-      //       icon: Get.isDarkMode ? const Icon(Icons.wb_sunny_outlined) : const Icon(Icons.dark_mode_outlined),
-      //       onPressed: () {
-      //         service.ThemeService().switchTheme();
-      //       },
-      //     ),
-      //   ],
-      // ),
       body: ListView(
         children: <Widget>[
           ListTile(
@@ -38,9 +23,10 @@ class _ChatScreenState extends State<ChatScreen> {
               backgroundColor: Get.isDarkMode ? theme.primaryClr : theme.redClr,
               child: Text('GC', style: theme.TextThemes.headlineSmall(context)),
             ),
-            title: Text('General Chat'),
-            subtitle: Text('Talk about anything and everything'),
-            trailing: Icon(Icons.arrow_forward_ios),
+            // TODO
+            title: const Text('General Chat'),
+            subtitle: const Text('Talk about anything and everything'),
+            trailing: const Icon(Icons.arrow_forward_ios),
           ),
         ],
       ),
