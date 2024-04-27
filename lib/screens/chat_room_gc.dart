2c077bc19e1a3187e6f65ee2996ff780ace3c0de
Sandro Aguilar <sunny.aguilar@gmail.com>
2024-04-26 23:53:56 -0700

    nak-app

--- lib/screens/chat_room_gc.dart
+++ lib/screens/chat_room_gc.dart
@@ -1,9 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
+import 'package:nak_app/ui/theme.dart' as theme;
 import 'package:nak_app/services/theme_service.dart' as service;
 
-class GeneralChatRoom extends StatelessWidget {
+class GeneralChatRoom extends StatefulWidget {
   const GeneralChatRoom({super.key});
+  @override
+  State<GeneralChatRoom> createState() => _GeneralChatRoomState();
+}
+
+class _GeneralChatRoomState extends State<GeneralChatRoom> {
+  final Stream<QuerySnapshot> _stream = FirebaseFirestore.instance.collection('chat').snapshots();
+
+  Center _circularProgress() {
+    return const Center(
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical:  60.0),
+        child: SizedBox(
+          height: 50, width: 50,
+          child: CircularProgressIndicator(
+            strokeWidth: 7,
+            color: theme.redClr,
+            backgroundColor: theme.bronzeOfficial,
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +43,34 @@ class GeneralChatRoom extends StatelessWidget {
           ),
         ],
       ),
-      body: const Placeholder(),
+
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal:  20.0),
+        child: Column(
+          children: <Widget>[
+            const Text('chat text 1'),
+            const SizedBox(height: 20,),
+            const Text('chat text 2'),
+            StreamBuilder(
+              stream: _stream,
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.data == null) { return _circularProgress(); }
+
+                if (!snapshot.hasData) { return _circularProgress(); }
+
+                if (snapshot.hasData) {
+                  print('Data: ${snapshot.data.docs.toList()}');
+                  print('Data: ${snapshot.data.docs.toList()[0]['initial']}');
+                  // print('Data: ${snapshot.data.docs.toList()[1]['second']}');
+                  return const Text('Data in streambuilder');
+                }
+                return _circularProgress();
+              }
+            ),
+
+          ],
+        ),
+      ),
     );
   }
 }
