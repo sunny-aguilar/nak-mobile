21f968062f3c91441b4710ed2cafb9263af5bbee
Sandro Aguilar <sunny.aguilar@gmail.com>
2024-05-02 00:19:51 -0700

    nak-app

---
+++ lib/ui/widget_export.dart
@@ -0,0 +1,5 @@
+// This file exports other files. When you import this file, it will automatically import
+// any files exported in the list below. This saves you from having a lot of import
+// statements in your files.
+
+export 'theme.dart';
