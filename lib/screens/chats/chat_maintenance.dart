4f731663c5c4904d507fd0ebcd868f60b7908381
Sandro Aguilar <sunny.aguilar@gmail.com>
2024-05-09 23:46:16 -0700

    nak-app

--- lib/screens/chats/chat_maintenance.dart
+++ lib/screens/chats/chat_maintenance.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:nak_app/services/theme_service.dart' as service;
 import 'package:nak_app/ui/theme.dart' as theme;
+import 'package:nak_app/services/theme_service.dart' as service;
 import 'package:nak_app/db/db_chat.dart' as db_chat;
 
-class ChatMaintenance extends StatelessWidget {
+class ChatMaintenance extends StatefulWidget {
   const ChatMaintenance({super.key});
+
+  @override
+  State<ChatMaintenance> createState() => _ChatMaintenanceState();
+}
+
+class _ChatMaintenanceState extends State<ChatMaintenance> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,6 +42,9 @@ class _ChatMaintenanceBodyState extends State<ChatMaintenanceBody> {
   // TODO: create variables to hold the new chat list that should update every cycle (3-5 days?)
   String chatListName = '';
 
+  // chat stat variables
+  int totalGcChats = 0;
+
   // TODO: create a function to change the chat list name every 72 hours
   void setNewChatListName() {
     DateTime start = DateTime(2024, 05, 01);
@@ -49,7 +58,7 @@ class _ChatMaintenanceBodyState extends State<ChatMaintenanceBody> {
   Center _circularProgress() {
     return const Center(
       child: SizedBox(
-        height: 75, width: 75,
+        height: 30, width: 30,
         child: CircularProgressIndicator(
           strokeWidth: 5, color: theme.redClr, backgroundColor: theme.lightGrey,
         ),
@@ -60,10 +69,13 @@ class _ChatMaintenanceBodyState extends State<ChatMaintenanceBody> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal:  24.0),
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
+          Text('Chat Room Stats', style: theme.TextThemes.headlineMedLarge(context),),
+          const SizedBox(height: 20,),
+          Text('General Chat Stats', style: theme.TextThemes.headlineSmall16(context),),
           FutureBuilder(
             future: db_chat.ChatSettings().totalChats('general_chat'),
             builder: (BuildContext context, snapshot) {
@@ -73,18 +85,59 @@ class _ChatMaintenanceBodyState extends State<ChatMaintenanceBody> {
 
                 List<dynamic> chatList = snapshot.data?['gc']['01'];
                 int totalChats = chatList.length;
-                print('val: $totalChats');
+                totalGcChats = totalChats;
 
-                return Text('1');
+                return ListView(
+                  shrinkWrap: true,
+                  children: [
+                    ListTile(
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => const GeneraChatOptions())
+                        );
+                      },
+                      leading: CircleAvatar(
+                        foregroundColor: Get.isDarkMode ? theme.darkGreyClr : theme.primaryClr,
+                        backgroundColor: Get.isDarkMode ? theme.primaryClr : theme.redClr,
+                        child: Text('$totalGcChats', style: theme.TextThemes.headlineSmall(context)),
+                      ),
+                      title: const Text('Total chat messages'),
+                      trailing: const Icon(Icons.arrow_forward_ios),
+                    ),
+                  ]
+                );
               }
               return _circularProgress();
             }
           ),
-          const Text('Chat Maintenance Body'),
           // TODO: add functions to add a new chat list
           // chatListName = utils.Dates().getDate();
-          const Text('Create New List'),
         ],
+      ),
+    );
+  }
+}
+
+class GeneraChatOptions extends StatelessWidget {
+  const GeneraChatOptions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Image.asset('assets/img/nak_letters_bw.png', height: 30.0,),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Chat Room Stats', style: theme.TextThemes.headlineMedLarge(context),),
+            const SizedBox(height: 20,),
+            const Text('Archive Chat'),
+          ],
+        ),
       ),
     );
   }
