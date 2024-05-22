6f09604593d65821d488092b0c5f29a07431d429
Sandro Aguilar <sunny.aguilar@gmail.com>
2024-05-22 14:49:48 -0700

    nak-app

--- lib/screens/chats/chat_rules.dart
+++ lib/screens/chats/chat_rules.dart
@@ -44,15 +44,13 @@ class _ChatRulesBodyState extends State<ChatRulesBody> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: RefreshIndicator(
-        onRefresh: _handleRefresh,
-        backgroundColor: theme.redClr,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            FutureBuilder(
+    return RefreshIndicator(
+      onRefresh: _handleRefresh,
+      backgroundColor: theme.redClr,
+      child: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          FutureBuilder(
             future: db.Chat().canChat(),
               builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
                 if (!snapshot.hasData) { _circularProgress(); }
@@ -76,87 +74,80 @@ class _ChatRulesBodyState extends State<ChatRulesBody> {
                 return _circularProgress();
               }
             ),
-            // Text('Chat Rules', style: theme.TextThemes.headlineLarge(context),),
-            ListView(
-              shrinkWrap: true,
-              children: <Widget>[
-                Text('Chat Rules', style: theme.TextThemes.headlineLarge(context), textAlign: TextAlign.center,),
-                const ListTile(
-                  leading: Icon(Icons.check_circle),
-                  title: Text('Be friendly. Be polite. Be considerate.',),
-                ),
-                const ListTile(
-                  leading: Icon(Icons.check_circle),
-                  title: Text('Do not type in all caps.'),
-                ),
-                const ListTile(
-                  leading: Icon(Icons.check_circle),
-                  title: Text('Do not verbally abuse, harrass, attack, embarrass, or threaten anyone else in the chat room, no matter what they might say to you'),
-                ),
-                const ListTile(
-                  leading: Icon(Icons.check_circle),
-                  title: Text('Do not use obscene, offensive, or sexually explicit language.'),
-                ),
-                const ListTile(
-                  leading: Icon(Icons.check_circle, color: theme.azureClr),
-                  title: Text('A blue bar means your chat priviledges are in good standing.',),
-                ),
-                const ListTile(
-                  leading: Icon(Icons.check_circle, color: theme.orangeClr),
-                  title: Text('An orange bar means your chat privileges have been suspended. Contact neb@nakinc.org if you believe this is incorrect.',),
-                ),
-                const SizedBox(height: 20,),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Text('Failure to follow any of these rules will result in you getting banned from chat.', textAlign: TextAlign.center, style: theme.TextThemes.chatWarning(context),),
-                ),
-                const SizedBox(height: 24,),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: TextButton(
-                    onPressed: () {
-                      if (chatEnabled) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            duration: Duration(milliseconds: 1500),
-                            content: Text('Verifying chat privileges...')
+          Text('Chat Rules', style: theme.TextThemes.headlineLarge(context), textAlign: TextAlign.center,),
+          const ListTile(
+            leading: Icon(Icons.check_circle),
+            title: Text('Be friendly. Be polite. Be considerate.',),
+          ),
+          const ListTile(
+            leading: Icon(Icons.check_circle),
+            title: Text('Do not type in all caps.'),
+          ),
+          const ListTile(
+            leading: Icon(Icons.check_circle),
+            title: Text('Do not verbally abuse, harrass, attack, embarrass, or threaten anyone else in the chat room, no matter what they might say to you'),
+          ),
+          const ListTile(
+            leading: Icon(Icons.check_circle),
+            title: Text('Do not use obscene, offensive, or sexually explicit language.'),
+          ),
+          const ListTile(
+            leading: Icon(Icons.check_circle, color: theme.azureClr),
+            title: Text('A blue bar means your chat priviledges are in good standing.',),
+          ),
+          const ListTile(
+            leading: Icon(Icons.check_circle, color: theme.orangeClr),
+            title: Text('An orange bar means your chat privileges have been suspended. Contact neb@nakinc.org if you believe this is incorrect.',),
+          ),
+          const SizedBox(height: 20,),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text('Failure to follow any of these rules will result in you getting banned from chat.', textAlign: TextAlign.center, style: theme.TextThemes.chatWarning(context),),
+          ),
+          const SizedBox(height: 24,),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: TextButton(
+              onPressed: () {
+                if (chatEnabled) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      duration: Duration(milliseconds: 1500),
+                      content: Text('Verifying chat privileges...')
+                    ),
+                  );
+                  Navigator.pushNamed(
+                    context,
+                    '/chat'
+                  );
+                }
+                else {
+                  showDialog<String>(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('Chat Disabled', textAlign: TextAlign.center,),
+                        content: const Text('Chat privileges are disabled.', textAlign: TextAlign.center,),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            style: buttons.buttonStyleRed(context),
+                            child: const Text('Dismiss'),
                           ),
-                        );
-                        Navigator.pushNamed(
-                          context,
-                          '/chat'
-                        );
-                      }
-                      else {
-                        showDialog<String>(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: const Text('Chat Disabled', textAlign: TextAlign.center,),
-                              content: const Text('Chat privileges are disabled.', textAlign: TextAlign.center,),
-                              actions: <Widget>[
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  style: buttons.buttonStyleRed(context),
-                                  child: const Text('Dismiss'),
-                                ),
-                              ],
-                            );
-                          }
-                        );
-                      }
-
-                    },
-                    style: buttons.chatButton(context, theme.redClr),
-                    child: const Text('Start Chatting', style: TextStyle(color: theme.primaryClr),),
-                  ),
-                ),
-              ],
+                        ],
+                      );
+                    }
+                  );
+                }
+          
+              },
+              style: buttons.chatButton(context, theme.redClr),
+              child: const Text('Start Chatting', style: TextStyle(color: theme.primaryClr),),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
