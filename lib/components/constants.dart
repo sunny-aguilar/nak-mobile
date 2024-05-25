2f0072e2d7a91493b2383397c45fe59d43a78f38
Sandro Aguilar <sunny.aguilar@gmail.com>
2024-05-25 08:20:08 -0700

    nak-app

--- lib/components/constants.dart
+++ lib/components/constants.dart
@@ -15,6 +15,3 @@ const appleApiKey = 'appl_api_key';
 
 //TO DO: add the Google API key for your app from the RevenueCat dashboard: https://app.revenuecat.com
 const googleApiKey = 'googl_api_key';
-
-//TO DO: add the Amazon API key for your app from the RevenueCat dashboard: https://app.revenuecat.com
-const amazonApiKey = 'amazon_api_key';
