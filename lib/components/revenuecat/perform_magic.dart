439070767719f8c9d7c01e53a6f68f043d4abd00
Sandro Aguilar <sunny.aguilar@gmail.com>
2024-05-27 21:19:52 -0700

    nak-app

--- lib/components/revenuecat/perform_magic.dart
+++ lib/components/revenuecat/perform_magic.dart
@@ -13,74 +13,74 @@ class RC extends StatefulWidget {
 }
 
 class _RCState extends State<RC> {
-
-  // show Paywall
-void performMagic(val) async {
   bool _isLoading = false;
 
-  setState(() {
-    _isLoading = true;
-  });
-
-  CustomerInfo customerInfo = await Purchases.getCustomerInfo();
-
-  if (customerInfo.entitlements.all[entitlementID] != null &&
-      customerInfo.entitlements.all[entitlementID]?.isActive == true) {
-    // if user has entitlement, allow premium function
-    setState(() {
-      _isLoading = false;
-    });
-  }
-  else {
-    Offerings? offerings;
-    try {
-      offerings = await Purchases.getOfferings();
-    }
-    on PlatformException catch (e) {
-      if (mounted) {
-        await showDialog(
-          context: context,
-          builder: (BuildContext context) => ShowDialogToDismiss(
-            title: 'Error',
-            content: e.message ?? 'Unknown error',
-            buttonText: 'OK'
-          )
-        );
-      }
-    }
+  // show Paywall
+  void performMagic(val) async {
 
     setState(() {
-      _isLoading = false;
+      _isLoading = true;
     });
 
-    if (offerings == null || offerings.current == null) {
-      // offerings are empty, show a message to user
+    CustomerInfo customerInfo = await Purchases.getCustomerInfo();
+
+    if (customerInfo.entitlements.all[entitlementID] != null &&
+        customerInfo.entitlements.all[entitlementID]?.isActive == true) {
+      // if user has entitlement, allow premium function
+      setState(() {
+        _isLoading = false;
+      });
     }
     else {
-      if (mounted) {
-        await showModalBottomSheet(
-          useRootNavigator: true,
-          isDismissible: true,
-          isScrollControlled: true,
-          backgroundColor: theme.kColorBackground,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(25.0)),
-          ),
-          context: context,
-          builder: (BuildContext context) {
-            return StatefulBuilder(
-              builder: (BuildContext context, StateSetter setModalState) {
-                return Paywall(
-                  offering: offerings!.current!,
-                );
-              }
-            );
-          }
-        );
+      Offerings? offerings;
+      try {
+        offerings = await Purchases.getOfferings();
+      }
+      on PlatformException catch (e) {
+        if (mounted) {
+          await showDialog(
+            context: context,
+            builder: (BuildContext context) => ShowDialogToDismiss(
+              title: 'Error',
+              content: e.message ?? 'Unknown error',
+              buttonText: 'OK'
+            )
+          );
+        }
+      }
+
+      setState(() {
+        _isLoading = false;
+      });
+
+      if (offerings == null || offerings.current == null) {
+        // offerings are empty, show a message to user
+      }
+      else {
+        if (mounted) {
+          await showModalBottomSheet(
+            useRootNavigator: true,
+            isDismissible: true,
+            isScrollControlled: true,
+            backgroundColor: theme.kColorBackground,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(25.0)),
+            ),
+            context: context,
+            builder: (BuildContext context) {
+              return StatefulBuilder(
+                builder: (BuildContext context, StateSetter setModalState) {
+                  return Paywall(
+                    offering: offerings!.current!,
+                  );
+                }
+              );
+            }
+          );
+        }
       }
     }
   }
-}
 
   @override
   Widget build(BuildContext context) {
