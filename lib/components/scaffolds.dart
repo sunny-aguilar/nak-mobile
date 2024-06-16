3011011c3c657d0bf0da21fb4e04a141c2b27ed8
Sandro Aguilar <sunny.aguilar@gmail.com>
2024-06-15 20:31:03 -0700

    nak-app

--- lib/components/scaffolds.dart
+++ lib/components/scaffolds.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
-import 'package:nak_app/screens/admin/users.dart' as users;
+import 'package:nak_app/screens/admin/users_permissions.dart' as users;
 import 'package:nak_app/ui/theme.dart' as theme;
 
 // return a floating action button for users who are admins
