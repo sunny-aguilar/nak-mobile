85ee952ffbd30c1e69a87a484616b04c97ba217c
Sandro Aguilar <sunny.aguilar@gmail.com>
2024-10-05 21:33:45 -0700

    nak-app

---
+++ lib/screens/bros.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+import 'package:nak_app/services/theme_service.dart' as service;
+import 'package:nak_app/ui/theme.dart' as theme;
+
+
+import 'package:flutter/material.dart';
+
+class BroScreen extends StatelessWidget {
+  const BroScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('USER PERMISSIONS', style: theme.TextThemes.headlineMedLarge(context),),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        actions: <Widget>[
+          IconButton(
+            icon: Get.isDarkMode ? const Icon(Icons.wb_sunny_outlined) : const Icon(Icons.dark_mode_outlined),
+            onPressed: () => service.ThemeService().switchTheme(),
+          ),
+        ],
+      ),
+    );
+  }
+}
