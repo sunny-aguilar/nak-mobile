4a17b33ef0f96a267a00864b1acfdd9f52bc2c55
Sandro Aguilar <sunny.aguilar@gmail.com>
2025-02-23 19:40:21 -0800

    nak-app

--- lib/screens/compliance/compliance_screen.dart
+++ lib/screens/compliance/compliance_screen.dart
@@ -23,117 +23,160 @@ class ComplianceScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          const SizedBox(height: 10,),
-          Stack(
-            // first element starts at the bottom of stack
-            children: <Widget>[
-              Container(color: theme.primaryClr.withAlpha(0), height: 280, width: double.infinity,),
-              Container(color: theme.redOfficial, height: 180, width: double.infinity,),
-              Positioned(
-                bottom: 60.0,
-                left: 0.0,
-                child: Container(
-                  height: 136, width: 110,
-                  decoration: const BoxDecoration(
-                    color: theme.bronzeOfficial,
-                    borderRadius: BorderRadius.only(topRight: Radius.circular(18.0), bottomRight: Radius.circular(18.0))
-                  ),
-                )
-              ),
-              Positioned(
-                top: 100.0,
-                left: 140.0,
-                // child: Text('Chapter Compliance', style: theme.TextThemes.headlineSmall20(context).copyWith(color: theme.primaryClr),)
-                child: RichText(
-                  text: TextSpan(
-                    children: <TextSpan>[
-                      TextSpan(text: 'Chapter Compliance\n', style: theme.TextThemes.headlineSmall20(context)),
-                      const TextSpan(text: '2024-2025 '),
-                      const TextSpan(text: 'Academic Year'),
-                    ]
-                  ),
-                )
-              )
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 30.0),
-            child: Text('Compliance Task', style: theme.TextThemes.oswaldText(context).copyWith(fontSize: 24.0),),
-          ),
-          Expanded(
-            child: ListView(
-              padding: const EdgeInsets.only(left: 30.0, top: 10.0, right: 24.0),
-              // shrinkWrap: true,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(height: 10,),
+            Stack(
+              // first element starts at the bottom of stack
               children: <Widget>[
-                ListTile(
-                  leading: const Icon(Icons.assessment), title: const Text('FOR CHAPTERS'),
-                  tileColor: theme.chatGregyClr,
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute<Widget>(
-                        builder: (BuildContext context) {
-                          return const ChapterComplianceScreen();
-                        }
-                      )
-                    );
-                  },
-                ),
-                const SizedBox(height: 5.0,),
-                ListTile(
-                  leading: const Icon(Icons.assessment), title: const Text('FOR COMPLIANCE VP'),
-                  tileColor: theme.chatGregyClr,
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute<Widget>(
-                        builder: (BuildContext context) {
-                          return const DirectorScreen();
-                        }
-                      )
-                    );
-                  },
-                ),
-                Container(
-                  // container used to give card a height
-                  height: 200,
-                  child: Card(
-                    color: theme.primaryClr,
-                    elevation: 5.0, // shadow
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                      side: BorderSide(color: theme.greyClr, width: 4.0),
+                Container(color: theme.primaryClr.withAlpha(0), height: 260, width: double.infinity,),
+                Container(color: theme.redOfficial, height: 180, width: double.infinity,),
+                Positioned(
+                  bottom: 40.0,
+                  left: 0.0,
+                  child: Container(
+                    height: 136, width: 110,
+                    decoration: const BoxDecoration(
+                      color: theme.bronzeOfficial,
+                      borderRadius: BorderRadius.only(topRight: Radius.circular(18.0), bottomRight: Radius.circular(18.0))
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Text('Chapters Start Here', style: theme.TextThemes.headlineMedLarge(context),),
-                    ),
-                  ),
+                  )
                 ),
-                Container(
-                  // container used to give card a height
-                  height: 200,
-                  child: Card(
-                    color: theme.primaryClr,
-                    elevation: 5.0, // shadow
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                      side: BorderSide(color: theme.greyClr, width: 4.0),
+                Positioned(
+                  top: 100.0,
+                  left: 140.0,
+                  // child: Text('Chapter Compliance', style: theme.TextThemes.headlineSmall20(context).copyWith(color: theme.primaryClr),)
+                  child: RichText(
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(text: 'Chapter Compliance\n', style: theme.TextThemes.headlineSmall20(context)),
+                        const TextSpan(text: '2024-2025 '),
+                        const TextSpan(text: 'Academic Year'),
+                      ]
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Text('Directors Start Here', style: theme.TextThemes.headlineMedLarge(context),),
-                    ),
-                  ),
-                ),
+                  )
+                )
               ],
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+              child: SizedBox(
+                // container used to give card a height
+                height: 200,
+                width: double.infinity,
+                child: Card(
+                  color: theme.primaryClr,
+                  elevation: 5.0, // shadow
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                    side: BorderSide(color: theme.greyClr, width: 4.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text('Chapters Start Here', style: theme.TextThemes.headlineMedLarge(context),),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox( height: 20,),
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+              child: SizedBox(
+                // container used to give card a height
+                height: 200,
+                width: double.infinity,
+                child: Card(
+                  color: theme.primaryClr,
+                  elevation: 5.0, // shadow
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                    side: BorderSide(color: theme.greyClr, width: 4.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text('Directors Start Here', style: theme.TextThemes.headlineMedLarge(context),),
+                  ),
+                ),
+              ),
+            ),
+            // Expanded(
+            //   child: ListView(
+            //     padding: const EdgeInsets.only(left: 30.0, top: 10.0, right: 30.0),
+            //     // shrinkWrap: true,
+            //     children: <Widget>[
+            //       ListTile(
+            //         leading: const Icon(Icons.assessment), title: const Text('FOR CHAPTERS'),
+            //         tileColor: theme.chatGregyClr,
+            //         onTap: () {
+            //           Navigator.push(
+            //             context,
+            //             MaterialPageRoute<Widget>(
+            //               builder: (BuildContext context) {
+            //                 return const ChapterComplianceScreen();
+            //               }
+            //             )
+            //           );
+            //         },
+            //       ),
+            //       const SizedBox(height: 5.0,),
+            //       ListTile(
+            //         leading: const Icon(Icons.assessment), title: const Text('FOR COMPLIANCE VP'),
+            //         tileColor: theme.chatGregyClr,
+            //         onTap: () {
+            //           Navigator.push(
+            //             context,
+            //             MaterialPageRoute<Widget>(
+            //               builder: (BuildContext context) {
+            //                 return const DirectorScreen();
+            //               }
+            //             )
+            //           );
+            //         },
+            //       ),
+            //       Padding(
+            //         padding: const EdgeInsets.only(left: 4.0),
+            //         child: Text('Compliance Task', style: theme.TextThemes.oswaldText(context).copyWith(fontSize: 24.0),),
+            //       ),
+            //       SizedBox(
+            //         // container used to give card a height
+            //         height: 200,
+            //         child: Card(
+            //           color: theme.primaryClr,
+            //           elevation: 5.0, // shadow
+            //           shape: const RoundedRectangleBorder(
+            //             borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            //             side: BorderSide(color: theme.greyClr, width: 4.0),
+            //           ),
+            //           child: Padding(
+            //             padding: const EdgeInsets.all(16.0),
+            //             child: Text('Chapters Start Here', style: theme.TextThemes.headlineMedLarge(context),),
+            //           ),
+            //         ),
+            //       ),
+            //       SizedBox(
+            //         // container used to give card a height
+            //         height: 200,
+            //         child: Card(
+            //           color: theme.primaryClr,
+            //           elevation: 5.0, // shadow
+            //           shape: const RoundedRectangleBorder(
+            //             borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            //             side: BorderSide(color: theme.greyClr, width: 4.0),
+            //           ),
+            //           child: Padding(
+            //             padding: const EdgeInsets.all(16.0),
+            //             child: Text('Directors Start Here', style: theme.TextThemes.headlineMedLarge(context),),
+            //           ),
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // ),
+          ],
+        ),
       ),
     );
   }
