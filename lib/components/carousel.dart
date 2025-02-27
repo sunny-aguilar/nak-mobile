119a01ff8c958d8f5c64cda3f5cd3f071e90ed89
Sandro Aguilar <sunny.aguilar@gmail.com>
2025-02-27 15:59:50 -0800

    nak-app

--- lib/components/carousel.dart
+++ lib/components/carousel.dart
@@ -13,7 +13,7 @@ class _CarouselComponentState extends State<CarouselComponent> {
   int activePage = 0;
 
   List<String> imagesArr = [
-    'assets/img/carousel/nakfest_carousel.webp',
+    'assets/img/carousel/nakfest_carousel.jpg',
     'assets/img/carousel/nldc_carousel.PNG',
     'assets/img/carousel/email_carousel.webp',
     'assets/img/carousel/store_carousel.webp',
