052e62bf8a7aadd51ffa480bfad89e8afc8c753d
Sandro Aguilar <sunny.aguilar@gmail.com>
2025-03-27 16:56:57 -0700

    nak-app

---
+++ lib/screens/directory/chart.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:flutter/material.dart';
+
+class BarGraph extends StatefulWidget {
+  const BarGraph({super.key});
+  @override
+  State<BarGraph> createState() => _BarGraphState();
+}
+
+class _BarGraphState extends State<BarGraph> {
+  List<double> chapterBrothers = [
+    100,120,90,150,110,70,50,110,130,140,120,80,90,60,100,130,70
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
