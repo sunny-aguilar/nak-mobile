7ccbb4f1313bbb36d6b0ed527912e7cd95119542
Sandro Aguilar <sunny.aguilar@gmail.com>
2025-03-27 17:23:49 -0700

    nak-app

--- lib/screens/directory/chart.dart
+++ lib/screens/directory/chart.dart
@@ -1,6 +1,39 @@
-
-
 import 'package:flutter/material.dart';
+
+
+class IndividualBar {
+  IndividualBar({required this.x, required this.y});
+  final int x;
+  final double y;
+}
+
+
+class BarData {
+  BarData({
+    required this.founding,
+    required this.alpha,
+    required this.beta,
+    required this.gamma,
+    required this.delta,
+    required this.epsilon,
+    required this.zeta,
+    required this.eta,
+    required this.theta,
+    required this.iota,
+  });
+  final double founding;
+  final double alpha;
+  final double beta;
+  final double gamma;
+  final double delta;
+  final double epsilon;
+  final double zeta;
+  final double eta;
+  final double theta;
+  final double iota;
+
+  List<IndividualBar> barData = [];
+}
 
 class BarGraph extends StatefulWidget {
   const BarGraph({super.key});
