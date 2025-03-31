1e805688d77d8cd4a556f6b5da864de219d279e3
Sandro Aguilar <sunny.aguilar@gmail.com>
2025-03-31 00:33:59 -0700

    nak-app

--- lib/screens/directory/directory.dart
+++ lib/screens/directory/directory.dart
@@ -108,13 +108,15 @@ class DirectoryDashboard extends StatefulWidget {
 }
 
 class _DirectoryDashboardState extends State<DirectoryDashboard> {
-  List<double> chapterBrothers = [
-    100,120,90,150,110,70,
-    50,110,130,140,120,80,
-    90,60,100,130,70,60,
-    110,120,90,150,80,130,
-    60,100,120,20
-  ];
+
+  // Graph data (replaced by widget.graphData)
+  // List<double> chapterBrothers = [
+  //   100,120,90,150,110,70,
+  //   50,110,130,140,120,80,
+  //   90,60,100,130,70,60,
+  //   110,120,90,150,80,130,
+  //   60,100,120,20
+  // ];
 
   @override
   Widget build(BuildContext context) {
