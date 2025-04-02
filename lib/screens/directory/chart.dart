89620984b7eb8b0f66cda048141ea9c7221f979f
Sandro Aguilar <sunny.aguilar@gmail.com>
2025-04-01 18:46:49 -0700

    nak-app

--- lib/screens/directory/chart.dart
+++ lib/screens/directory/chart.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:get/get.dart';
 import 'package:nak_app/ui/theme.dart' as theme;
